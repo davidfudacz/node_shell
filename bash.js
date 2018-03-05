3c9d7a394a1e173adc8b1d22720d009819642f3c
Daniel Simandl <dsimandl419@gmail.com>
2018-03-05 16:29:30 -0600

    final push

--- bash.js
+++ bash.js
@@ -1,18 +1,26 @@
 const commands = require('./commands.js');
 
 // Output a prompt
-process.stdout.write('prompt > ');
+const prompt = '\nprompt > '
+process.stdout.write(prompt);
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
+
   var cmd = data.toString().trim(); // remove the newline
   let cmdSplit = cmd.split(' ');
   let cmdActual = cmdSplit[0];
   let args = cmdSplit.slice(1);
   if (commands[cmdActual]) {
-    commands[cmdActual](args);
+    commands[cmdActual](args, done);
   }
   else process.stdout.write('You typed: ' + cmd);
-  
+
 
 });
+
+function done (output) {
+
+  process.stdout.write(output + prompt);
+
+}
--- commands.js
+++ commands.js
@@ -2,73 +2,49 @@ const fs = require('fs');
 
 
 module.exports = {
-  pwd: function () {
-    let arr = process.argv[1].split('/').slice(0,-1).join('/');
-
-    process.stdout.write(arr);
-    process.stdout.write('\nprompt > ');
+  pwd: function (file, done) {
+    done(process.cwd());
 
   },
-  date: function () {
-    let date = new Date;
-    
-    process.stdout.write(date.toString());
-    process.stdout.write('\nprompt > ');
-
+  date: function (file, done) {
+    done(Date());
   },
-  cat: function (file) {
-    let prefix = process.argv[1].split('/').slice(0,-1).join('/');
-    
-    fs.readFile(prefix+'/'+file[0], (err, data) => {
+  cat: function (file, done) {
+    fs.readFile(file[0],'utf-8', (err, data) => {
       if (err) throw err;
-
-      process.stdout.write(data);
-
+      done(data)
     });
-    process.stdout.write('\nprompt > ');
-
   },
-  head: function (file) {
-    let prefix = process.argv[1].split('/').slice(0,-1).join('/');
-    fs.readFile(prefix+'/'+file[0], 'utf-8', (err, data) => {
+  head: function (file, done) {
+    fs.readFile(file[0], 'utf-8', (err, data) => {
       if (err) throw err;
-
-      let output = data.toString();
-      console.log(output);
-      // let newArr= Array.prototype.slice.call(data);
-      // let output = newArr.join(' ').split(/\b(10)\b/g);
-      // output = output.slice(0,11).join('');
-      
-      // console.log(JSON.stringify(data, null, 5));
-    
-      // process.stdout.write(output.toString());
-
+      done(data.split('\n').slice(0,5).join('\n'));
     });
-    process.stdout.write('\nprompt > ');
 
   },
-  echo: function (arg) {
-    let possibleArg = arg[0][0];
-    let output;
-    switch (possibleArg) {
-      case '$': 
-        if (arg[0] === '$') output = arg.join(' ');
-        let envirVar = arg[0].slice(1);
-        output = process.env[envirVar];
-        break;
-
-      case '-':
-        break;
-      default: output = arg.join(' ');
-
-    }
-    
-    
-    process.stdout.write(output);
-    process.stdout.write('\nprompt > ');
-
+  tail(file, done) {
+    fs.readFile(file[0], 'utf-8', (err, data) => {
+      if (err) throw err;
+      done(data.split('\n').slice(-5).join('\n'));
+    });
   },
-  ls: function () {
+  echo: function (args, done) {
+
+    let output = args
+    .map(arg => {
+        if (arg[0] === '$') {
+          let envirVar = arg.slice(1);
+          let output = process.env[envirVar];
+          return output;
+        } else {
+          return arg;
+        }
+      })
+    .join(' ');
+
+    done(output);
+  },
+  ls: function (file, done) {
     fs.readdir('.', function(err, files) {
       if (err) throw err;
       files.forEach(function(file) {
@@ -81,18 +57,3 @@ module.exports = {
 }
 
 
-  // if (cmd === 'pwd') {
-  //   let arr = process.argv[1].split('/').slice(0,-1).join('/');
-
-  //   process.stdout.write(arr);
-  // }
-  // else if (cmd === 'date') {
-  //   let date = new Date;
-    
-  //   process.stdout.write(date.toString());
-
-  // }
-
-  // else {
-
-  // }
